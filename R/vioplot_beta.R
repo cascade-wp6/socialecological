26c450db5397666dd08469023a84be3ee72cb657
Florian Schneider <florian.schneider@univ-montp2.fr>
2016-12-06 14:08:31 +0100

    add vioplot with beta kernel

---
+++ R/vioplot_beta.R
@@ -0,0 +1,128 @@
+#' Title
+#'
+#' @param x 
+#' @param ... 
+#' @param range 
+#' @param h 
+#' @param ylim 
+#' @param names 
+#' @param horizontal 
+#' @param col 
+#' @param border 
+#' @param lty 
+#' @param lwd 
+#' @param rectCol 
+#' @param colMed 
+#' @param pchMed 
+#' @param at 
+#' @param add 
+#' @param wex 
+#' @param drawRect 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' 
+#' vioplot_beta(summary(L100)$feed)
+
+vioplot_beta <- function (x, ..., range = 1.5, h = NULL, ylim = NULL, names = NULL, 
+          horizontal = FALSE, col = "white", border = "black", lty = 1, 
+          lwd = 1, rectCol = "black", colMed = "white", pchMed = 19, 
+          at, add = FALSE, wex = 1, drawRect = TRUE) 
+{
+  datas <- list(x, ...)
+  n <- length(datas)
+  if (missing(at)) 
+    at <- 1:n
+  upper <- vector(mode = "numeric", length = n)
+  lower <- vector(mode = "numeric", length = n)
+  q1 <- vector(mode = "numeric", length = n)
+  q3 <- vector(mode = "numeric", length = n)
+  med <- vector(mode = "numeric", length = n)
+  base <- vector(mode = "list", length = n)
+  height <- vector(mode = "list", length = n)
+  baserange <- c(Inf, -Inf)
+  args <- list(display = "none")
+  if (!(is.null(h))) 
+    args <- c(args, h = h)
+  for (i in 1:n) {
+    data <- datas[[i]]
+    data.min <- min(data)
+    data.max <- max(data)
+    q1[i] <- quantile(data, 0.25)
+    q3[i] <- quantile(data, 0.75)
+    med[i] <- median(data)
+    iqd <- q3[i] - q1[i]
+    upper[i] <- min(q3[i] + range * iqd, data.max)
+    lower[i] <- max(q1[i] - range * iqd, data.min)
+    est.xlim <- c(min(lower[i], data.min), max(upper[i], 
+                                               data.max))
+    smout <- do.call("betakernel", list(data))  
+    hscale <- 0.4/max(smout$y) * wex
+    base[[i]] <- smout$x
+    height[[i]] <- smout$y * hscale
+    t <- range(base[[i]])
+    baserange[1] <- min(baserange[1], t[1])
+    baserange[2] <- max(baserange[2], t[2])
+  }
+  if (!add) {
+    xlim <- if (n == 1) 
+      at + c(-0.5, 0.5)
+    else range(at) + min(diff(at))/2 * c(-1, 1)
+    if (is.null(ylim)) {
+      ylim <- baserange
+    }
+  }
+  if (is.null(names)) {
+    label <- 1:n
+  }
+  else {
+    label <- names
+  }
+  boxwidth <- 0.05 * wex
+  if (!add) 
+    plot.new()
+  if (!horizontal) {
+    if (!add) {
+      plot.window(xlim = xlim, ylim = ylim)
+      axis(2)
+      axis(1, at = at, label = label)
+    }
+    box()
+    for (i in 1:n) {
+      polygon(c(at[i] - height[[i]], rev(at[i] + height[[i]])), 
+              c(base[[i]], rev(base[[i]])), col = col, border = border, 
+              lty = lty, lwd = lwd)
+      if (drawRect) {
+        lines(at[c(i, i)], c(lower[i], upper[i]), lwd = lwd, 
+              lty = lty)
+        rect(at[i] - boxwidth/2, q1[i], at[i] + boxwidth/2, 
+             q3[i], col = rectCol)
+        points(at[i], med[i], pch = pchMed, col = colMed)
+      }
+    }
+  }
+  else {
+    if (!add) {
+      plot.window(xlim = ylim, ylim = xlim)
+      axis(1)
+      axis(2, at = at, label = label)
+    }
+    box()
+    for (i in 1:n) {
+      polygon(c(base[[i]], rev(base[[i]])), c(at[i] - height[[i]], 
+                                              rev(at[i] + height[[i]])), col = col, border = border, 
+              lty = lty, lwd = lwd)
+      if (drawRect) {
+        lines(c(lower[i], upper[i]), at[c(i, i)], lwd = lwd, 
+              lty = lty)
+        rect(q1[i], at[i] - boxwidth/2, q3[i], at[i] + 
+               boxwidth/2, col = rectCol)
+        points(med[i], at[i], pch = pchMed, col = colMed)
+      }
+    }
+  }
+  invisible(list(upper = upper, lower = lower, median = med, 
+                 q1 = q1, q3 = q3))
+}
