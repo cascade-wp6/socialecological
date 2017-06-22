33811a45eb6e3cf5b697db15ccc8700d25eb7c54
Florian Schneider <florian.schneider@univ-montp2.fr>
2017-06-22 15:19:47 +0200

    vectorise feed() function

--- R/feed.R
+++ R/feed.R
@@ -8,12 +8,20 @@
 #' @export
 
 feed <- function(l, parms = livestock$parms, factor = 1) {
-
-    rho_one <- as.vector(sum(l$cells == "1")/(l$dim[1]*l$dim[2])) # get initial vegetation cover
-    q_one_one <- neighbors(l, "1")/4  # count local density of occupied fields for each cell
   
-    temp <- with(parms,  ( (a+ v*q_one_one) * (1-p*q_one_one) * L * rho_one^(1+q) )/( 1 + (a+ v*q_one_one) * (1-p*q_one_one) * h * rho_one^(1+q) ) )   # get probability of death by grazing for each cell, i.e. productivity of fodder
-    
-    area <- prod(l$dim)*0.25  #area of landscape object
-    sum(temp[l$cells == "1"])*factor/area  
+    if("landscape_list" %in% class(l)) {
+      
+      sapply(L, function(l) feed(l, parms = parms)  )
+      
+      } else {
+        
+        rho_one <- as.vector(sum(l$cells == "1")/(l$dim[1]*l$dim[2])) # get initial vegetation cover
+        q_one_one <- neighbors(l, "1")/4  # count local density of occupied fields for each cell
+        
+        temp <- with(parms,  ( (a+ v*q_one_one) * (1-p*q_one_one) * L * rho_one^(1+q) )/( 1 + (a+ v*q_one_one) * (1-p*q_one_one) * h * rho_one^(1+q) ) )   # get probability of death by grazing for each cell, i.e. productivity of fodder
+        
+        area <- prod(l$dim)*0.25  #area of landscape object
+        sum(temp[l$cells == "1"])*factor/area  
+    }
+  
 }
