3c2564a2ac12140330dae423940600c4ba50652b
Florian Schneider <florian.schneider@univ-montp2.fr>
2017-06-22 15:20:09 +0200

    special case for kernel function for all zero vector

--- R/betakernel.R
+++ R/betakernel.R
@@ -1,4 +1,3 @@
-
 #' Title
 #'
 #' @param x 
@@ -9,9 +8,11 @@
 #' @return a list of x and y values. The y values describe the expected frequency of observations at value x. 
 #' @export
 
-betakernel <- function(x, n = 512, lambda = 0.02,  bcmethod= "beta1") {
+betakernel <- function(x, n = 512, lambda = 0.01, bw = NULL,  bcmethod= "beta1") {
   out <- list()
   out$x <- seq(0,1, length = 512)
-  out$y <- dbckden(out$x, x, lambda = lambda,  bcmethod= bcmethod, xmax = 1)
+  if(all(x == 0)) {out$y = c(n, rep(0, n-1))} else {
+    out$y <- evmix::dbckden(out$x, x, lambda = lambda, bw = bw, bcmethod= bcmethod, xmax = 1)
+  }
   return(out)
 }
